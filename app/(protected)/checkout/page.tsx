dc2e059abe5e004ec0157da8cfc517ec25d75b61
Shane Hubsch <shanehubsch@gmail.com>
2025-04-11 13:09:39 +0200

    'OrderSummaryCheckout' cannot be used as a JSX bug

--- app/(protected)/checkout/page.tsx
+++ app/(protected)/checkout/page.tsx
@@ -1,11 +1,21 @@
 import React from "react"
 import { redirect } from "next/navigation"
 import { getUserCart } from "@/server/actions"
+import { HelpCircle } from "lucide-react"
 import { getServerSession } from "next-auth"
 
 import { RedisCartItem } from "@/types/cart-item"
 import { authOptions } from "@/lib/auth/auth-options"
-import OrderSummaryCheckout from "@/components/PageCheckout/OrderSummaryCheckout"
+import { products } from "@/lib/product-attributes"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import CartItemCardCheckout from "@/components/PageCheckout/CartItemCardCheckout"
 import ShippingDetails from "@/components/PageCheckout/ShippingDetails"
 
 export default async function CheckoutPage() {
@@ -16,6 +26,15 @@ export default async function CheckoutPage() {
   }
   const cartItems = (await getUserCart()) as RedisCartItem[]
 
+  const shipping = products[0].priceShipping
+  let subTotal = 0
+  if (cartItems && cartItems.length > 0) {
+    for (let i = 0; i < cartItems.length; i++) {
+      subTotal = subTotal + cartItems[i].totalPrice
+    }
+  }
+  let total = subTotal + shipping
+
   return (
     <section className="container mb-10 min-h-screen items-center bg-background">
       <div className="mx-auto h-full w-11/12 pt-10 md:w-10/12">
@@ -23,9 +42,44 @@ export default async function CheckoutPage() {
           <div className="lg:col-span-2">
             <ShippingDetails />
           </div>
-          <div>
-            <OrderSummaryCheckout cartItems={cartItems} />
-          </div>
+          <Card>
+            <CardHeader>
+              <CardTitle>Order Summary</CardTitle>
+              <CardDescription>Get ready for explosive fun!</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <div className="space-y-4">
+                <div className="flex flex-col items-start gap-4">
+                  {cartItems &&
+                    cartItems.map((item) => (
+                      <CartItemCardCheckout key={item.id} cartItem={item} />
+                    ))}
+                </div>
+                <div className="space-y-2">
+                  <div className="flex justify-between">
+                    <div className="flex items-center gap-2">
+                      <span>Subtotal (Incl. VAT)</span>
+                      <HelpCircle className="size-4 text-muted-foreground" />
+                    </div>
+                    <span>R {subTotal}.00</span>
+                  </div>
+                  <div className="flex justify-between">
+                    <span>Shipping</span>
+                    <span>R {shipping}.00</span>
+                  </div>
+                  <div className="flex justify-between border-t pt-2 font-medium">
+                    <span>Total</span>
+                    <span>R {total}.00</span>
+                  </div>
+                </div>
+                <div className="flex justify-end">
+                  <Button variant="default" className="lg:w-full">
+                    Continue to Payment
+                  </Button>
+                </div>
+              </div>
+            </CardContent>
+          </Card>
         </div>
       </div>
     </section>
